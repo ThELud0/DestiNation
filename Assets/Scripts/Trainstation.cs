650c2fe31c25ad146f8fd881c9fa4b347102596b
Juliette Bourdiau <juliette.bourdiau@gmail.com>
2024-11-30 11:53:48 +0100

    base trains

--- Assets/Scripts/Trainstation.cs
+++ Assets/Scripts/Trainstation.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Trainstation : MonoBehaviour
 {
     float destinyTimer;
-    float trainTimer;
+    float lifetime;
+    int destinyType;
+
+    [SerializeField] GameObject rockstarTrain;
+    [SerializeField] GameObject oldTrain;
+    [SerializeField] GameObject dictatorTrain;
+    [SerializeField] GameObject lambdaTrain;
     void Start()
     {
-        destinyTimer = Time.time + Random.Range(10, 30);
-        trainTimer = Random.Range(2, 6);
+        destinyTimer = Time.time + Random.Range(3, 10);
     }
 
     void Update()
@@ -15,11 +21,77 @@ public class Trainstation : MonoBehaviour
         if (Time.time >= destinyTimer)
         {
             changeNature();
+
+            if (destinyType == 0)
+            {
+                SpawnLambdaTrain();
+            }
+            if (destinyType == 1)
+            {
+                SpawnDictatorTrain();
+            }
+            if (destinyType == 2)
+            {
+                SpawnRockstarTrain();
+            }
+            if (destinyType == 3)
+            {
+                SpawnOldTrain();
+            }
         }
     }
 
     void changeNature()
     {
-        //GetComponent<MeshRenderer>().material.mainTexture ;
+        float rdm = Random.Range(0, 100);
+        if (rdm < 45)
+        {
+            GetComponent<MeshRenderer>().material.color =  Color.blue;
+            destinyType = 0;
+        }
+        else if (rdm < 55)
+        {
+            GetComponent<MeshRenderer>().material.color =  Color.red;
+            destinyType = 1;
+        }
+        else if (rdm < 75)
+        {
+            GetComponent<MeshRenderer>().material.color = Color.yellow;
+            destinyType = 2;
+        }
+        else if (rdm >= 75)
+        {
+            GetComponent<MeshRenderer>().material.color = Color.grey;
+            destinyType = 3;
+        }
+        destinyTimer = Time.time + Random.Range(10, 30);
+    }
+
+    void SpawnRockstarTrain()
+    {
+        GameObject train = Instantiate(rockstarTrain, transform.position, Quaternion.identity);
+        Train trainComponent = train.GetComponent<Train>();
+        trainComponent.Initialize(9, 12);
+    }
+
+    void SpawnOldTrain()
+    {
+        GameObject train = Instantiate(oldTrain, transform.position, Quaternion.identity);
+        Train trainComponent = train.GetComponent<Train>();
+        trainComponent.Initialize(Random.Range(34, 40), 5);
+    }
+
+    void SpawnDictatorTrain()
+    {
+        GameObject train = Instantiate(dictatorTrain, transform.position, Quaternion.identity);
+        Train trainComponent = train.GetComponent<Train>();
+        trainComponent.Initialize(Random.Range(14, 26), 6);
+    }
+
+    void SpawnLambdaTrain()
+    {
+        GameObject train = Instantiate(lambdaTrain, transform.position, Quaternion.identity);
+        Train trainComponent = train.GetComponent<Train>();
+        trainComponent.Initialize(Random.Range(14, 26), 8);
     }
 }
