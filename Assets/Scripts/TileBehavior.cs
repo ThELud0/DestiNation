0a7cadefdd3d33bc1fa200de703604c0468576cd
Mett0t <100679801+Mett0t@users.noreply.github.com>
2024-12-01 14:50:09 +0100

    testing rail deletion

--- Assets/Scripts/TileBehavior.cs
+++ Assets/Scripts/TileBehavior.cs
@@ -1,34 +1,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using static UnityEditor.FilePathAttribute;
 
 
 public class TileBehavior : MonoBehaviour
 {
 
     [SerializeField]
-    private GameObject railPrefab;
-    [SerializeField]
-    private GameObject railTurnPrefab;
+    private GameObject railPrefab, railTurnPrefab, IntersectionQuadrupleEnCroixRM;
     Vector3 additionnalHeight = new Vector3(0f, 1f, 0f);
+    GameObject createdRail;
+    GameObject intersectionRail;
+
 
     public void CreateRail(Quaternion rotation)
     {
-        GameObject rail = Instantiate(railPrefab, transform.position + additionnalHeight, rotation);
-        
-
+        if(createdRail == null)
+        {
+            createdRail = Instantiate(railPrefab, transform.position + additionnalHeight, rotation);
+        }
+        else
+        {
+            Destroy(createdRail);
+            intersectionRail = Instantiate(IntersectionQuadrupleEnCroixRM, transform.position + additionnalHeight, rotation);
+        }
     }
 
     public void CreateTurnRail(Quaternion rotation)
     {
-        Instantiate(railTurnPrefab, transform.position + additionnalHeight, rotation);
+        createdRail = Instantiate(railTurnPrefab, transform.position + additionnalHeight, rotation);
     }
 
 
     public void CheckPath(Vector2 currentPos, Vector2 nextPos, Vector2 previousPos)
     {
-
-        //Vector2 turnDir = nextPos - currentPos;
         Quaternion rotation = Quaternion.identity;
 
         if (currentPos.y > previousPos.y)
@@ -63,7 +69,7 @@ public class TileBehavior : MonoBehaviour
             else if (currentPos.y > nextPos.y)
             {
                 //bg
-                rotation = Quaternion.Euler(0f, -90f, 0f);
+                rotation = Quaternion.Euler(0f, 0f, 0f);
                 CreateTurnRail(rotation);
             }
             else if (currentPos.x < nextPos.x)
@@ -115,23 +121,27 @@ public class TileBehavior : MonoBehaviour
                 CreateRail(rotation);
             }
         }
-
-
-
-      
-
-
-
-
-
     }
+    public void DeleteRail()
+    {
+        if (createdRail != null)
+        {
+            Destroy(createdRail);
+            createdRail = null;
+        }
+        if (intersectionRail != null)
+        {
+            Destroy(intersectionRail);
+            intersectionRail = null;
+            createdRail = Instantiate(railPrefab, transform.position + additionnalHeight, Quaternion.identity);
+        }
     }
-    //var monrail
 
-    //connaitre son rail
-    //on m'ajoute une direction
-    //on m'enlève une direction
+    public GameObject RailReturn()
+    {
+        return createdRail;
+    }
+}
 
-    //update le rail
 
 
