5ace0f07d0388a6634bf882af14857bc9c04f374
Justine9999999 <justine932010@live.fr>
2024-12-01 15:49:59 +0100

    l

--- Assets/Scripts/PathFinderTool.cs
+++ Assets/Scripts/PathFinderTool.cs
@@ -27,17 +27,30 @@ public class PathFinderTool : MonoBehaviour
 
     public Levelgenerator levelGeneratorInstance;
     
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        foreach(Vector2 step in list_tuiles_path)
+        {
+            Gizmos.DrawSphere(new Vector3(step.x, 3f, step.y), 0.2f);
+        }
+    }
 
 void Update(){
     if (Mouse.current.leftButton.wasPressedThisFrame)
         {
-      mouse_clicked();
+      //mouse_clicked_on_something();
             
+        }
+
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            OnClickOnTile();
         }
 }
 
 
-     void mouse_clicked()
+   /*  void mouse_clicked_on_something()
    {
 
         Debug.Log("mouuuuse");
@@ -74,46 +87,167 @@ void Update(){
                 }
             }
 
-        }
-        RaycastHit hit2;
+        }*/
+       /* RaycastHit hit2;
         if(Physics.Raycast(ray, out hit2, 100)){
             Debug.Log("begin Path");
 
             AddToPathOfTuile(hit2.transform.position, false);
         }else{
             Debug.Log("not detected");
+        }*/
+   //}
+
+
+   void OnClickOnTile()
+   {
+        Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+        RaycastHit[] hits = Physics.RaycastAll(ray, 100, unspawnableZone);
+
+        print("hit number : " + hits.Length);
+
+        if (hits.Length > 0) 
+        {
+            foreach(RaycastHit hit in hits)
+             {
+                Debug.Log("the tag is "+hit.transform.gameObject.tag);
+                if(hit.transform.gameObject.tag == "Trainstation"){
+                    trainChanged(hit.transform.gameObject.GetComponent<Trainstation>());
+                    Debug.Log("trainnnn");
+                    return;
+                }
+
+                if(hit.transform.gameObject.tag == "Human"){
+                    Vector2 pos_Human = new Vector2(hit.transform.position.x, hit.transform.position.z);
+                    Vector2 diff = (pos_Human - list_tuiles_path[list_tuiles_path.Count-1]).normalized;
+                    if (diff== new Vector2(0,1) || diff==new Vector2(1,0)){
+                    pathCompleted();
+                    Debug.Log("baaby");
+                    }
+
+
+                    return;
+                }
+
+                if(hit.transform.gameObject.tag == "Structure"){
+                    resetpath();
+                    Debug.Log("on annulle");
+
+                    return;
+                }
+
+                // if(precedent_pos_mouse_pressed==Vector2.zero){
+                //     return;
+                // }
+            }
+            Debug.Log("on add");
+
+            if (current_trainstation) 
+            {
+                Vector3 hitPosition = hits[0].transform.position;
+                Vector2 hitCoordinates =  new Vector3 (hitPosition.x, hitPosition.z);
+
+                Vector2 direction = (hitCoordinates - precedent_pos_mouse_pressed).normalized;
+                
+
+                if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+                {
+                    direction = new Vector2(direction.x/Mathf.Abs(direction.x), 0);
+                }
+                else 
+                {
+                    direction = new Vector2(0, direction.y/Mathf.Abs(direction.y));
+                }
+
+                int depth = Mathf.Abs(direction.x != 0 
+                ? Mathf.RoundToInt(precedent_pos_mouse_pressed.x - hitCoordinates.x) 
+                : Mathf.RoundToInt(precedent_pos_mouse_pressed.y - hitCoordinates.y));
+
+                AddTuileToPathWithoutPathFinding(direction, depth);
+            }
+        }
+   }
+
+   void AddTuileToPathWithoutPathFinding(Vector2 direction, int depth)
+   { 
+        for (int i = 1; i <= depth; i++)
+        {
+            Vector2 position = precedent_pos_mouse_pressed + direction ;
+            if (!isTileFreeForRail(position)) break;
+            precedent_pos_mouse_pressed = position;
+            list_tuiles_path.Add(position);
+            if(isAHuman(position)) 
+            {
+                pathCompleted();
+                return;
+            }
         }
    }
 
 
-     bool isTileFreeForRail(Vector2 tested_vec){
-        transform.position = new Vector3(tested_vec.x, 3, tested_vec.y);
-        Ray ray_test = new Ray(transform.position, Vector3.down);
-        RaycastHit[] hits =  Physics.RaycastAll(ray_test, 2f);
-            if (hits.Length > 0)
+   bool isAHuman(Vector2 tested_vec)
+     {
+        Vector3 origin = new Vector3(tested_vec.x, 3, tested_vec.y);
+        Ray ray_test = new Ray(origin, Vector3.down);
+        RaycastHit[] hits =  Physics.RaycastAll(ray_test, 100f, unspawnableZone);
+
+        Debug.DrawLine(origin, origin + Vector3.down * 100, Color.red, 10);
+        print(hits.Length);
+        if (hits.Length > 0)
+        {
+            foreach(RaycastHit hit in hits)
             {
-                foreach(RaycastHit hit in hits){
-                                    Debug.Log("Spotted "+ hit.transform.gameObject.tag);
+                Debug.Log("Ssssspotted "+ hit.transform.gameObject.tag);
 
-               foreach(string tag in list_String_unspawnable_baby){
+                
 
-                if(tag == hit.transform.gameObject.tag){
-                     Debug.Log("Spotted a structure at baby speculation point !");
-
-                    return false;
-                }
-               }
-               }
-        
-            }else {
+                    if(hit.transform.gameObject.TryGetComponent<Human>(out Human h))
+                    {
+                        Debug.Log("Structure block");
+                        return true;
+                    }
+                
             }
-            
-            foreach(Vector2 vec in list_tuiles_path){
-                if(vec==tested_vec){
-                    return false;
+    
+        }
+        return false;
+    }
+   
+
+
+     bool isTileFreeForRail(Vector2 tested_vec)
+     {
+        Vector3 origin = new Vector3(tested_vec.x, 3, tested_vec.y);
+        Ray ray_test = new Ray(origin, Vector3.down);
+        RaycastHit[] hits =  Physics.RaycastAll(ray_test, 100f, unspawnableZone);
+
+        Debug.DrawLine(origin, origin + Vector3.down * 100, Color.red, 10);
+        print(hits.Length);
+        if (hits.Length > 0)
+        {
+            foreach(RaycastHit hit in hits)
+            {
+                Debug.Log("Ssssspotted "+ hit.transform.gameObject.tag);
+
+                foreach(string tag in list_String_unspawnable_baby)
+                {
+
+                    if(tag == hit.transform.gameObject.tag)
+                    {
+                        Debug.Log("Structure block");
+                        return false;
+                    }
                 }
             }
-            return true;
+    
+        }
+        foreach(Vector2 vec in list_tuiles_path){
+            if(vec==tested_vec){
+                return false;
+            }
+        }
+        return true;
     }
    
 
@@ -208,7 +342,7 @@ void Update(){
     }
 
     private void Visualize(Vector2 pos){
-            //Instantiate(visualitor, new Vector3(pos.x, 2, pos.y), Quaternion.identity);
+            Instantiate(visualitor, new Vector3(pos.x, 2, pos.y), Quaternion.identity);
     }
 
     private void pathCompleted(){
